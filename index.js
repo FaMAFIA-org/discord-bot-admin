79d56ae73531aa41cbbd6bd791ad3f95ad1a5310
juani mancini <juani.mancini98.com>
2023-09-04 15:35:29 -0300

    log when a message is send

--- index.js
+++ index.js
@@ -1,55 +1,65 @@
-import { readdirSync } from 'node:fs';
+import * as fs from 'fs';
 import { join } from 'node:path';
 import { Client, Collection, Events, GatewayIntentBits } from 'discord.js';
 import 'dotenv/config';
 import path from 'node:path';
 import { fileURLToPath } from 'node:url';
+import { log } from 'node:console';
 
 const token = process.env.DISCORD_TOKEN
 
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages] });
 
 client.commands = new Collection();
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const foldersPath = join(__dirname, 'commands');
-const commandFolders = readdirSync(foldersPath);
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
+// const commandsPath = join(__dirname, 'commands');
+// const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
 
-for (const folder of commandFolders) {
-	const commandsPath = join(foldersPath, folder);
-	const commandFiles = readdirSync(commandsPath).filter(file => file.endsWith('.js'));
-	for (const file of commandFiles) {
-		const filePath = join(commandsPath, file);
-		const command = require(filePath);
-		if ('data' in command && 'execute' in command) {
-			client.commands.set(command.data.name, command);
-		} else {
-			console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
-		}
-	}
-}
+// // debug
+// console.log(commandFiles);
+// console.log(commandsPath);
 
-client.once(Events.ClientReady, () => {
-	console.log('Ready!');
-});
+// for (const file of commandFiles) {
+// 	const filePath = path.join(commandsPath, file);
+// 	import * as command from filepath;
+// 	// Set a new item in the Collection with the key as the command name and the value as the exported module
+// 	if ('data' in command && 'execute' in command) {
+// 		client.commands.set(command.data.name, command);
+// 	} else {
+// 		console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+// 	}
+// }
 
-client.on(Events.InteractionCreate, async interaction => {
-	if (!interaction.isChatInputCommand()) return;
+// client.once(Events.ClientReady, () => {
+// 	console.log('Ready!');
+// });
 
-	const command = client.commands.get(interaction.commandName);
+// client.on(Events.InteractionCreate, async interaction => {
+// 	if (!interaction.isChatInputCommand()) return;
 
-	if (!command) return;
+// 	const command = client.commands.get(interaction.commandName);
 
-	try {
-		await command.execute(interaction);
-	} catch (error) {
-		console.error(error);
-		if (interaction.replied || interaction.deferred) {
-			await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true });
-		} else {
-			await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
-		}
-	}
-});
+// 	if (!command) return;
+
+// 	try {
+// 		await command.execute(interaction);
+// 	} catch (error) {
+// 		console.error(error);
+// 		if (interaction.replied || interaction.deferred) {
+// 			await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true });
+// 		} else {
+// 			await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+// 		}
+// 	}
+// });
 
 client.login(token);
+
+client.on('ready', () => {
+	console.log(`${client.user.tag}`)
+});
+
+client.on('messageCreate', (message) => {
+	console.log(message.content);
+})
