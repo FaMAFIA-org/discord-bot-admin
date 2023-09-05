73a46ea575a879b75e2e28c1d857731a235701c7
juan <juani.mancini98@gmail.com>
2023-09-05 20:55:23 -0300

    refactor index

--- index.js
+++ index.js
@@ -1,39 +1,55 @@
 import { REST } from '@discordjs/rest';
-import { WebSocketManager } from '@discordjs/ws';
-import {
-	GatewayDispatchEvents,
-	GatewayIntentBits,
-	Client,
-} from '@discordjs/core';
-import 'dotenv/config';
+import { GatewayIntentBits, Client } from '@discordjs/core';
+import { config } from 'dotenv';
 
-// commands import
-import pingFunction from './commands/new-ping.js';
-// Create REST and WebSocket managers directly
-const rest = new REST({ version: '10' }).setToken(
-	process.env.NODE_DISCORD_TOKEN,
-);
+config();
 
-const gateway = new WebSocketManager({
-	token: process.env.NODE_DISCORD_TOKEN,
-	intents: GatewayIntentBits.GuildMessages | GatewayIntentBits.MessageContent,
-	rest,
+// Comandos
+const commands = [
+	{
+	  name: 'ping',
+	  description: 'Replies with Pong!',
+	},
+  ];
+
+const TOKEN = process.env.NODE_DISCORD_TOKEN;
+const CLIENT_ID = process.env.NODE_DISCORD_APP_ID
+
+// Esto nos dice que partes necesitamos pedirle a la API
+const client = new Client({
+	intents: [
+		GatewayIntentBits.Guilds,
+		GatewayIntentBits.GuildMessages,
+		GatewayIntentBits.MessageContent, // Esto requiere activar los intents desde el bot
+	],
 });
 
+const rest = new REST({version: 10}).setToken(TOKEN)
 
-// Create a client to emit relevant events.
-const client = new Client({ rest, gateway });
 
-client.on(Events.ShardError, error => {
-	console.error('A websocket connection encountered an error:', error);
+client.on('ready', () => {
+	console.log(`the ${client.user.tag} bot has logged in!`);
+})
+
+client.on('interactionCreate', async interaction => {
+	if (!interaction.isChatInputCommand()) return;
+	
+	if (interaction.commandName === 'ping') {
+		await interaction.reply('Pong!');
+	}
 });
 
-// Listen for interactions
-// Each event contains an `api` prop along with the event data that allows you to interface with the Discord REST API
-client.on(GatewayDispatchEvents.InteractionCreate, pingFunction);
+async function main () {
+	try {
+		console.log('Started refreshing application (/) commands.');
+		await rest.put(Routes.applicationCommands(CLIENT_ID), { body: commands });
+		console.log('Successfully reloaded application (/) commands.');	
+	} catch (err) {
+		console.log(err);
+	}
+};
 
-// Listen for the ready event
-client.once(GatewayDispatchEvents.Ready, () => console.log('Ready!'));
+// log in bot
+client.login(TOKEN);
 
-// Start the WebSocket connection.
-gateway.connect();
+main();
