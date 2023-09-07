ff85c02d95c105a48dd8b5968b8058e37355a281
juani mancini <juani.mancini98.com>
2023-09-07 18:52:05 -0300

    basic warning command

--- index.js
+++ index.js
@@ -9,22 +9,30 @@ const commands = [
 		name: 'ping',
 		description: 'Replies with Pong!',
 	},
+	{
+		name: 'warning',
+		description: 'cuidadito che...',
+		options: [
+			{
+				name: 'user',
+				description: 'usuario',
+				type: 6,
+				required: true,
+			},
+			{
+				name: 'motivo',
+				description: 'infraccion',
+				type: 3,
+				required: true,
+			}
+		]
+	}
 ];
 
-const TOKEN = process.env.NODE_DISCORD_TOKEN;
+const TOKEN = process.env.DISCORD_TOKEN;
 const CLIENT_ID = process.env.NODE_DISCORD_APP_ID;
 
 const rest = new REST({ version: '10' }).setToken(TOKEN);
-
-try {
-	console.log('Started refreshing application (/) commands.');
-
-	await rest.put(Routes.applicationCommands(CLIENT_ID), { body: commands });
-
-	console.log('Successfully reloaded application (/) commands.');
-} catch (error) {
-	console.error(error);
-}
 
 // Esto nos dice que partes necesitamos pedirle a la API
 const client = new Client({
@@ -39,12 +47,25 @@ client.on('ready', () => {
 	console.log(`the ${client.user.tag} bot has logged in!`);
 })
 
+try {
+	console.log('Started refreshing application (/) commands.');
+
+	await rest.put(Routes.applicationCommands(CLIENT_ID), { body: commands });
+
+	console.log('Successfully reloaded application (/) commands.');
+} catch (error) {
+	console.error(error);
+}
+
 // handle slash commands
 client.on('interactionCreate', async interaction => {
 	if (!interaction.isChatInputCommand()) return;
 
 	if (interaction.commandName === 'ping') {
 		await interaction.reply('Pong!');
+	} else if (interaction.commandName === 'warning') {
+		console.log(interaction.options.get('user'))
+		await interaction.reply(`ahora @${interaction.options.get('user').user.tag} se gano un warning por ${interaction.options.get('motivo').value}`)
 	}
 });
 
