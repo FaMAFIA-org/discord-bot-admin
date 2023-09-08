8ce0976b7c77ee7c8d9157b3aa3ab0e3fe48cecf
Juan Mancini <jmancini@atentus.com>
2023-09-08 12:15:21 -0300

    added command modularity

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
+import { Client,Collection,GatewayIntentBits,REST,Routes } from 'discord.js';
 import { config } from 'dotenv';
-import { REST, Routes, Client, GatewayIntentBits } from 'discord.js';
+import * as fs from 'node:fs';
+import path from 'node:path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 config();
+const TOKEN = process.env.DISCORD_TOKEN;
+const CLIENT_ID = process.env.NODE_DISCORD_APP_ID;
 
 // Comandos
 const commands = [
@@ -29,9 +37,6 @@ const commands = [
 	}
 ];
 
-const TOKEN = process.env.DISCORD_TOKEN;
-const CLIENT_ID = process.env.NODE_DISCORD_APP_ID;
-
 const rest = new REST({ version: '10' }).setToken(TOKEN);
 
 // Esto nos dice que partes necesitamos pedirle a la API
@@ -43,14 +48,29 @@ const client = new Client({
 	],
 });
 
-client.on('ready', () => {
-	console.log(`the ${client.user.tag} bot has logged in!`);
-})
+client.commands = new Collection();
+
+const commandsPath = path.join(__dirname,'commands')
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+	const filePath = path.join(commandsPath,file);
+	const { default: command } = await import(filePath);
+	console.log(filePath);
+	// Set a new item in the Collection with the key as the command name and the value as the exported module
+	if ('data' in command && 'execute' in command) {
+		client.commands.set(command.data.name,command);
+	} else {
+		console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+	}
+}
+
+
 
 try {
 	console.log('Started refreshing application (/) commands.');
 
-	await rest.put(Routes.applicationCommands(CLIENT_ID), { body: commands });
+	await rest.put(Routes.applicationCommands(CLIENT_ID),{ body: commands });
 
 	console.log('Successfully reloaded application (/) commands.');
 } catch (error) {
@@ -58,7 +78,7 @@ try {
 }
 
 // handle slash commands
-client.on('interactionCreate', async interaction => {
+client.on('interactionCreate',async interaction => {
 	if (!interaction.isChatInputCommand()) return;
 
 	if (interaction.commandName === 'ping') {
@@ -69,6 +89,11 @@ client.on('interactionCreate', async interaction => {
 	}
 });
 
-// // log in bot
+// avisarnos que el bot se logueo
+client.on('ready',() => {
+	console.log(`the ${client.user.tag} bot has logged in!`);
+})
+
+// log in bot
 client.login(TOKEN);
 
